9d124982df3dccf922d9effc039c676953f24709
leekangmin <kkk5474096@naver.com>
2023-06-25 19:47:02 +0900

    [CHORE/#4] gradle groovy -> kotlin DSL

---
+++ app/build.gradle.kts
@@ -0,0 +1,102 @@
+plugins {
+    id("com.android.application")
+    kotlin("android")
+    kotlin("kapt")
+    id("kotlin-parcelize")
+    id("dagger.hilt.android.plugin")
+}
+
+android {
+    namespace = Constants.packageName
+    compileSdk = Constants.compileSdk
+
+    defaultConfig {
+        applicationId = Constants.packageName
+        minSdk = Constants.minSdk
+        targetSdk = Constants.targetSdk
+        versionCode = Constants.versionCode
+        versionName = Constants.versionName
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = Versions.javaVersion
+        targetCompatibility = Versions.javaVersion
+    }
+
+    kotlinOptions {
+        jvmTarget = Versions.jvmVersion
+    }
+
+    buildFeatures {
+        dataBinding = true
+        viewBinding = true
+    }
+}
+
+dependencies {
+    KotlinDependencies.run {
+        implementation(kotlin)
+        implementation(coroutines)
+        implementation(jsonSerialization)
+        implementation(dateTime)
+    }
+
+    AndroidXDependencies.run {
+        implementation(coreKtx)
+        implementation(appCompat)
+        implementation(constraintLayout)
+        implementation(fragment)
+        implementation(startup)
+        implementation(legacy)
+        implementation(security)
+        implementation(hilt)
+        implementation(lifeCycleKtx)
+        implementation(lifecycleJava8)
+        implementation(splashScreen)
+        implementation(pagingRuntime)
+        implementation(workManager)
+        implementation(hiltWorkManager)
+    }
+
+    KaptDependencies.run {
+        kapt(hiltCompiler)
+        kapt(hiltWorkManagerCompiler)
+    }
+
+    implementation(MaterialDesignDependencies.materialDesign)
+
+    TestDependencies.run {
+        testImplementation(jUnit)
+        androidTestImplementation(androidTest)
+        androidTestImplementation(espresso)
+    }
+
+    ThirdPartyDependencies.run {
+        implementation(coil)
+        implementation(platform(okHttpBom))
+        implementation(okHttp)
+        implementation(okHttpLoggingInterceptor)
+        implementation(retrofit)
+        implementation(retrofitJsonConverter)
+        implementation(timber)
+        implementation(ossLicense)
+        implementation(progressView)
+        implementation(balloon)
+        implementation(lottie)
+        debugImplementation(flipperLeakCanary)
+        debugImplementation(leakCanary)
+        debugImplementation(soloader)
+    }
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,4 +13,4 @@ dependencyResolutionManagement {
     }
 }
 rootProject.name = "YELLO"
-include ':app'
+include(":app")

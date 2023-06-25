1c72f03b1fdd9f8d1bb543a5e12ac7ed6f196e31
leekangmin <kkk5474096@naver.com>
2023-06-25 19:46:25 +0900

    [CHORE/#4] BuildSrc

---
+++ buildSrc/src/main/kotlin/Constants.kt
@@ -0,0 +1,8 @@
+object Constants {
+    const val packageName = "com.yello"
+    const val compileSdk = 33
+    const val minSdk = 28
+    const val targetSdk = 33
+    const val versionCode = 1
+    const val versionName = "1.0"
+}
---
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -0,0 +1,50 @@
+import org.gradle.api.JavaVersion
+
+object Versions {
+    const val buildToolsVersion = "30.0.3"
+
+    const val kotlinVersion = "1.8.20"
+    const val kotlinSerializationJsonVersion = "1.5.1"
+    const val kotlinDateTimeVersion = "0.4.0"
+
+    const val coreKtxVersion = "1.10.1"
+    const val appCompatVersion = "1.6.1"
+    const val materialDesignVersion = "1.6.1"
+    const val constraintLayoutVersion = "2.1.4"
+    const val appStartUpVersion = "1.1.1"
+    const val legacySupportVersion = "1.0.0"
+    const val hiltVersion = "2.46.1"
+    const val activityKtxVersion = "1.7.2"
+    const val fragmentKtxVersion = "1.5.7"
+    const val coroutinesAndroidVersion = "1.7.1"
+    const val pagingVersion = "3.1.1"
+    const val securityVersion = "1.0.0"
+    const val lifecycleVersion = "2.6.1"
+    const val ossPluginVersion = "0.10.4"
+    const val ossVersion = "17.0.0"
+    const val splashVersion = "1.0.1"
+    const val composeVersion = "1.4.3"
+    const val workManagerVersion = "2.8.1"
+
+    const val coilVersion = "2.4.0"
+    const val retrofitVersion = "2.9.0"
+    const val kotlinSerializationConverterVersion = "1.0.0"
+    const val okHttpVersion = "4.11.0"
+    const val timberVersion = "5.0.1"
+    const val progressViewVersion = "1.1.3"
+    const val balloonVersion = "1.4.5"
+    const val lottieVersion = "6.0.0"
+
+    const val gradleVersion = "8.0.1"
+
+    const val junitVersion = "4.13.2"
+    const val espressoVersion = "3.3.0"
+    const val androidTestVersion = "1.1.2"
+
+    const val flipperVersion = "0.193.0"
+    const val soloaderVersion = "0.10.5"
+    const val leakCanaryVersion = "2.11"
+
+    val javaVersion = JavaVersion.VERSION_17
+    const val jvmVersion = "17"
+}

cb061dd31580dba72d790d6adcfc9d9db82d4e8a
leekangmin <kkk5474096@naver.com>
2023-06-27 23:49:34 +0900

    [ADD/#1] AppModule Provide

---
+++ app/src/main/java/com/yello/di/AppModule.kt
@@ -0,0 +1,20 @@
+package com.yello.di
+
+import android.content.Context
+import com.example.data.util.FileParser
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+    @Provides
+    @Singleton
+    fun provideFileParser(
+        @ApplicationContext context: Context
+    ): FileParser = FileParser(context)
+}

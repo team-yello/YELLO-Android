f4b58ac1151e2b31489c1978bfcf415827579df5
b1urrrr <b1urrr@naver.com>
2023-06-29 04:48:56 +0900

    [ADD/#7] BindingActivity hide keyboard 처리

--- app/src/main/java/com/yello/di/RetrofitModule.kt
+++ app/src/main/java/com/yello/di/RetrofitModule.kt
@@ -7,6 +7,7 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 import kotlinx.serialization.json.Json
 import okhttp3.Interceptor
 import okhttp3.MediaType.Companion.toMediaType
@@ -14,11 +15,12 @@ import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Converter
 import retrofit2.Retrofit
-import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object RetrofitModule {
+    private const val APPLICATION_JSON = "application/json"
+
     @Provides
     @Singleton
     fun provideJson(): Json = Json {
@@ -29,7 +31,7 @@ object RetrofitModule {
     @Provides
     @Singleton
     fun provideJsonConverter(json: Json): Converter.Factory =
-        json.asConverterFactory("application/json".toMediaType())
+        json.asConverterFactory(APPLICATION_JSON.toMediaType())
 
     @Provides
     @Singleton
--- core-ui/src/main/java/com/example/ui/base/BindingActivity.kt
+++ core-ui/src/main/java/com/example/ui/base/BindingActivity.kt
@@ -1,10 +1,13 @@
 package com.example.ui.base
 
 import android.os.Bundle
+import android.view.MotionEvent
+import android.view.View
 import androidx.annotation.LayoutRes
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
+import com.example.ui.context.hideKeyboard
 
 abstract class BindingActivity<T : ViewDataBinding>(
     @LayoutRes private val layoutResId: Int,
@@ -15,5 +18,10 @@ abstract class BindingActivity<T : ViewDataBinding>(
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, layoutResId)
         binding.lifecycleOwner = this
+    }
+
+    override fun dispatchTouchEvent(ev: MotionEvent?): Boolean {
+        hideKeyboard(currentFocus ?: View(this))
+        return super.dispatchTouchEvent(ev)
     }
 }

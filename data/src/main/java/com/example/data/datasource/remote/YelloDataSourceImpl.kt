21879b48ce74ff914f3684aa5128ea742a830aea
leekangmin <kkk5474096@naver.com>
2023-06-29 23:04:48 +0900

    [FEAT/#6] YelloDataSource & Impl

---
+++ data/src/main/java/com/example/data/datasource/remote/YelloDataSourceImpl.kt
@@ -0,0 +1,31 @@
+package com.example.data.datasource.remote
+
+import com.example.data.datasource.YelloDataSource
+import com.example.data.model.response.BaseResponse
+import com.example.data.model.response.yello.ResponseCheckKeyword
+import com.example.data.model.response.yello.ResponseCheckName
+import com.example.data.model.response.yello.ResponseMyYello
+import com.example.data.model.response.yello.ResponseYelloDetail
+import com.example.data.remote.service.YelloService
+import javax.inject.Inject
+
+class YelloDataSourceImpl @Inject constructor(
+    private val service: YelloService
+): YelloDataSource {
+    override suspend fun getMyYelloList(page: Int): BaseResponse<ResponseMyYello> {
+        return service.getMyYelloList(page)
+    }
+
+    override suspend fun getYelloDetail(id: Long): BaseResponse<ResponseYelloDetail> {
+        return service.getYelloDetail(id)
+    }
+
+    override suspend fun checkKeyword(id: Long): BaseResponse<ResponseCheckKeyword> {
+        return service.checkKeyword(id)
+    }
+
+    override suspend fun checkName(id: Long): BaseResponse<ResponseCheckName> {
+        return service.checkName(id)
+    }
+
+}

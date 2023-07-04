f850b2d18991b1d3bd4db11629f03f2ae4ad53c0
Sangho Kim <marchbreeze03@gmail.com>
2023-07-05 03:39:33 +0900

    [ADD/#12] SignIn Activity 추가

---
+++ app/src/main/java/com/yello/presentation/auth/signInActivity.kt
@@ -0,0 +1,12 @@
+package com.yello.presentation.auth
+
+import android.os.Bundle
+import com.example.ui.base.BindingActivity
+import com.yello.R
+import com.yello.databinding.ActivitySignInBinding
+
+class SignInActivity : BindingActivity<ActivitySignInBinding>(R.layout.activity_sign_in) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}

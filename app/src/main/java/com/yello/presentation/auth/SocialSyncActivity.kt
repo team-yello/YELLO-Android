e40b078b90c12a6df2603f3e6979ad3f983ea033
Sangho Kim <marchbreeze03@gmail.com>
2023-07-05 15:34:17 +0900

    [ADD/#12] SocialSync Activity 추가

---
+++ app/src/main/java/com/yello/presentation/auth/SocialSyncActivity.kt
@@ -0,0 +1,12 @@
+package com.yello.presentation.auth
+
+import android.os.Bundle
+import com.example.ui.base.BindingActivity
+import com.yello.R
+import com.yello.databinding.ActivitySocialSyncBinding
+
+class SocialSyncActivity : BindingActivity<ActivitySocialSyncBinding>(R.layout.activity_social_sync) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}

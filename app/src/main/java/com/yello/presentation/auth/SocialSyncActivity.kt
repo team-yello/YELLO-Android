de3f7fcda2f354c978a4e241851a59e2b6b80e7b
Sangho Kim <marchbreeze03@gmail.com>
2023-07-07 03:05:39 +0900

    [FEAT/#20] 친구목록 불러와서 리스트로 저장 구현

--- app/src/main/java/com/yello/presentation/auth/SocialSyncActivity.kt
+++ app/src/main/java/com/yello/presentation/auth/SocialSyncActivity.kt
@@ -2,22 +2,32 @@ package com.yello.presentation.auth
 
 import android.os.Bundle
 import com.example.ui.base.BindingActivity
-import com.yello.R
-import com.yello.databinding.ActivitySocialSyncBinding
+import com.example.ui.view.setOnSingleClickListener
 import com.kakao.sdk.talk.TalkApiClient
 import com.kakao.sdk.talk.model.Friend
+import com.yello.R
+import com.yello.databinding.ActivitySocialSyncBinding
 import timber.log.Timber
 
-class SocialSyncActivity : BindingActivity<ActivitySocialSyncBinding>(R.layout.activity_social_sync) {
+class SocialSyncActivity :
+    BindingActivity<ActivitySocialSyncBinding>(R.layout.activity_social_sync) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        binding.btnSignIn.setOnSingleClickListener {
+            getFriendsList()
+        }
+    }
+
+    private fun getFriendsList() {
         TalkApiClient.instance.friends { friends, error ->
             if (error != null) {
                 Timber.tag("signIn").e(error, "카카오 프로필 가져오기 실패")
-            }
-            else if (friends != null) {
-                val friendList : List<Friend>? = friends.elements
+            } else if (friends != null) {
+                val friendList: List<Friend>? = friends.elements
+                val friendIdList = friendList?.map { friend -> friend.id }
+            } else {
+                Timber.tag("signIn").e("연동 가능한 카카오톡 친구 없음")
             }
         }
     }

63d55cf62acdb858b4cd59958243102c4e8f3311
pmj1459 <76741702+minju1459@users.noreply.github.com>
2023-07-08 19:25:27 +0900

    [FEAT/#19] 학교 검색 bottomsheet adapter 구현중

--- app/src/main/java/com/yello/presentation/onboarding/fragment/school/SearchDialogFragment.kt
+++ app/src/main/java/com/yello/presentation/onboarding/fragment/school/SearchDialogFragment.kt
@@ -6,10 +6,18 @@ import com.example.ui.base.BindingBottomSheetDialog
 import com.yello.R
 import com.yello.databinding.FragmentDialogSchoolBinding
 
-class SearchDialogFragment : BindingBottomSheetDialog<FragmentDialogSchoolBinding>(R.layout.fragment_dialog_school){
+class SearchDialogFragment : BindingBottomSheetDialog<FragmentDialogSchoolBinding>(R.layout.fragment_dialog_school) {
 
+    private var schoolAdpapter: SchoolAdpapter? = null
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        initSchoolAdapter()
+    }
+
+    private fun initSchoolAdapter() {
+        schoolAdpapter = SchoolAdpapter()
+        binding.rvSchoollist.adapter = schoolAdpapter
     }
     companion object {
         @JvmStatic

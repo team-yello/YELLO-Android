348db4edff291f3e277b0ab48114307e37aba8a1
Sangho Kim <marchbreeze03@gmail.com>
2023-07-10 16:46:07 +0900

    [ADD/#22] 학교 추천 친구 화면 구현

--- app/src/main/java/com/yello/presentation/main/recommend/RecommendSchoolFragment.kt
+++ app/src/main/java/com/yello/presentation/main/recommend/RecommendSchoolFragment.kt
@@ -2,6 +2,7 @@ package com.yello.presentation.main.recommend
 
 import android.os.Bundle
 import android.view.View
+import androidx.fragment.app.viewModels
 import com.example.ui.base.BindingFragment
 import com.example.ui.view.setOnSingleClickListener
 import com.yello.R
@@ -9,8 +10,15 @@ import com.yello.databinding.FragmentRecommendSchoolBinding
 
 class RecommendSchoolFragment : BindingFragment<FragmentRecommendSchoolBinding>(R.layout.fragment_recommend_school) {
 
+    private val viewModel by viewModels<RecommendSchoolViewModel>()
+    private val recommendAdapter = RecommendAdapter()
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        viewModel.addListFromLocal()
+        recommendAdapter.submitList(viewModel.recommendResult.value)
+        binding.rvRecommendSchool.adapter = recommendAdapter
 
         binding.layoutInviteFriend.setOnSingleClickListener {
 

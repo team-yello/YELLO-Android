a8aadadd2308c4e1548b692927a5fedefd7b1bef
Sangho Kim <marchbreeze03@gmail.com>
2023-07-11 02:06:31 +0900

    [UI/#22] 다이얼로그 UI 제작

---
+++ app/src/main/java/com/yello/presentation/main/recommend/RecommendInviteDialog.kt
@@ -0,0 +1,17 @@
+package com.yello.presentation.main.recommend
+
+import android.os.Bundle
+import android.view.View
+import com.example.ui.base.BindingDialogFragment
+import com.yello.R
+import com.yello.databinding.FragmentRecommendInviteDialogBinding
+
+class RecommendInviteDialog :
+    BindingDialogFragment<FragmentRecommendInviteDialogBinding>(R.layout.fragment_recommend_invite_dialog) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+}

9fa0de58b9a7fdf4d8bcbababea92470eb9c295b
Sangho Kim <marchbreeze03@gmail.com>
2023-07-12 00:37:08 +0900

    [UI/#28] 프로필 회원 탈퇴 UI 추가

---
+++ app/src/main/java/com/yello/presentation/main/profile/ProfileQuitForSureFragment.kt
@@ -0,0 +1,16 @@
+package com.yello.presentation.main.profile
+
+import android.os.Bundle
+import android.view.View
+import com.example.ui.base.BindingFragment
+import com.yello.R
+import com.yello.databinding.FragmentProfileQuitForSureBinding
+
+class ProfileQuitForSureFragment :
+    BindingFragment<FragmentProfileQuitForSureBinding>(R.layout.fragment_profile_quit_for_sure) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
---
+++ app/src/main/java/com/yello/presentation/main/profile/ProfileQuitFragment.kt
@@ -0,0 +1,16 @@
+package com.yello.presentation.main.profile
+
+import android.os.Bundle
+import android.view.View
+import com.example.ui.base.BindingFragment
+import com.yello.R
+import com.yello.databinding.FragmentProfileQuitBinding
+
+class ProfileQuitFragment :
+    BindingFragment<FragmentProfileQuitBinding>(R.layout.fragment_profile_quit) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}

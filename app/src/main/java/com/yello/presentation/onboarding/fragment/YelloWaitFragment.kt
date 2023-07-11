89001829818188a8eb319403cc304917f6755e71
b1urrrr <b1urrr@naver.com>
2023-07-11 12:27:35 +0900

    [ADD/#18] 옐로하기 프래그먼트 생성

---
+++ app/src/main/java/com/yello/presentation/onboarding/fragment/YelloWaitFragment.kt
@@ -0,0 +1,20 @@
+package com.yello.presentation.onboarding.fragment
+
+import android.os.Bundle
+import android.view.View
+import com.example.ui.base.BindingFragment
+import com.yello.R
+import com.yello.databinding.FragmentYelloWaitBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class YelloWaitFragment : BindingFragment<FragmentYelloWaitBinding>(R.layout.fragment_yello_wait) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance() = YelloWaitFragment()
+    }
+}

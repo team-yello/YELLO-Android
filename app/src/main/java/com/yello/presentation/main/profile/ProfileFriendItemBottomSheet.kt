70cbf3bc56011ece145878a15f4344a88c6eea03
Sangho Kim <marchbreeze03@gmail.com>
2023-07-12 19:51:53 +0900

    [UI/#28] 친구 바텀시트 뷰 UI 설정

---
+++ app/src/main/java/com/yello/presentation/main/profile/ProfileFriendItemBottomSheet.kt
@@ -0,0 +1,16 @@
+package com.yello.presentation.main.profile
+
+import android.os.Bundle
+import android.view.View
+import com.example.ui.base.BindingBottomSheetDialog
+import com.yello.R
+import com.yello.databinding.FragmentDialogSchoolBinding
+
+class SearchDialogFragment :
+    BindingBottomSheetDialog<FragmentDialogSchoolBinding>(R.layout.fragment_dialog_school) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}

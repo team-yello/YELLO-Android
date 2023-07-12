5d8639ceb855622dd98db4afcc077a352e83a856
Leekangmin <kkk5474096@naver.com>
2023-07-12 19:05:37 +0900

    [FEAT/#13] MyApp & AppModule ResolutionMetrics

--- app/src/main/java/com/yello/MyApp.kt
+++ app/src/main/java/com/yello/MyApp.kt
@@ -3,14 +3,24 @@ package com.yello
 import android.app.Application
 import com.kakao.sdk.common.KakaoSdk
 import com.yello.BuildConfig.NATIVE_APP_KEY
+import com.yello.presentation.util.ResolutionMetrics
 import dagger.hilt.android.HiltAndroidApp
+import javax.inject.Inject
 
 @HiltAndroidApp
 class MyApp : Application() {
+    @Inject
+    lateinit var metrics: ResolutionMetrics
+
     override fun onCreate() {
         super.onCreate()
 
         setUpFlipper()
         KakaoSdk.init(this, NATIVE_APP_KEY)
+        MyApp.resolutionMetrics = metrics
+    }
+
+    companion object {
+        lateinit var resolutionMetrics: ResolutionMetrics
     }
 }

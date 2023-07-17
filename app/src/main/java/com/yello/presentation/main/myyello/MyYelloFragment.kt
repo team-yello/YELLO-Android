f9b02f0f7a6fe5b73d9cdd27311dc0def0a6affd
Leekangmin <kkk5474096@naver.com>
2023-07-17 18:52:47 +0900

    [FEAT/#42] Log 삭제

--- app/src/main/java/com/yello/presentation/main/myyello/MyYelloFragment.kt
+++ app/src/main/java/com/yello/presentation/main/myyello/MyYelloFragment.kt
@@ -1,7 +1,6 @@
 package com.yello.presentation.main.myyello
 
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.flowWithLifecycle

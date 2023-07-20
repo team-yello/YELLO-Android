cae510814b3b93f429cf30e0126d5cff5df4d91c
pmj1459 <76741702+minju1459@users.noreply.github.com>
2023-07-20 17:35:50 +0900

    [FIX/#51] git splashActivity intent

--- app/src/main/java/com/yello/presentation/main/SplashActivity.kt
+++ app/src/main/java/com/yello/presentation/main/SplashActivity.kt
@@ -15,7 +15,7 @@ class SplashActivity : AppCompatActivity() {
         setContentView(R.layout.activity_splash)
 
         Handler(Looper.getMainLooper()).postDelayed({
-            val intent = Intent(this, OnBoardingActivity::class.java)
+            val intent = Intent(this, SignInActivity::class.java)
             startActivity(intent)
             finish()
         }, 3000)

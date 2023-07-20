f29555c6f00aad91ff152f072f853ad91a75c78d
pmj1459 <76741702+minju1459@users.noreply.github.com>
2023-07-20 19:48:45 +0900

    [FIX/#51] bottomsheetdialog full screen

--- app/src/main/java/com/yello/presentation/onboarding/fragment/school/university/SearchDialogSchoolFragment.kt
+++ app/src/main/java/com/yello/presentation/onboarding/fragment/school/university/SearchDialogSchoolFragment.kt
@@ -1,11 +1,15 @@
 package com.yello.presentation.onboarding.fragment.school.university
 
-import android.annotation.SuppressLint
 import android.content.Intent
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.net.Uri
 import android.os.Bundle
 import android.view.MotionEvent
 import android.view.View
+import android.view.ViewGroup
+import android.view.WindowManager
+import android.widget.LinearLayout
 import androidx.core.widget.doAfterTextChanged
 import androidx.fragment.app.activityViewModels
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -14,6 +18,7 @@ import com.example.ui.base.BindingBottomSheetDialog
 import com.example.ui.context.hideKeyboard
 import com.example.ui.view.UiState
 import com.example.ui.view.setOnSingleClickListener
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.yello.R
 import com.yello.databinding.FragmentDialogSchoolBinding
 import com.yello.presentation.onboarding.activity.OnBoardingViewModel
@@ -22,8 +27,8 @@ import com.yello.util.context.yelloSnackbar
 class SearchDialogSchoolFragment :
     BindingBottomSheetDialog<FragmentDialogSchoolBinding>(R.layout.fragment_dialog_school) {
     private var adapter: SchoolAdapter? = null
-
     private val viewModel by activityViewModels<OnBoardingViewModel>()
+    private lateinit var bottomSheetBehavior: BottomSheetBehavior<LinearLayout>
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -33,6 +38,13 @@ class SearchDialogSchoolFragment :
         setListWithInfinityScroll()
         recyclerviewScroll()
         seClicktoSchoolform()
+
+        dialog?.window?.setLayout(
+            ViewGroup.LayoutParams.MATCH_PARENT,
+            ViewGroup.LayoutParams.MATCH_PARENT,
+        )
+        dialog?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        dialog?.window?.clearFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND)
     }
 
     private fun initView() {
@@ -94,8 +106,6 @@ class SearchDialogSchoolFragment :
         viewModel.clearSchoolData()
         dismiss()
     }
-
-    @SuppressLint("ClickableViewAccessibility")
     private fun recyclerviewScroll() {
         binding.rvSchoolList.setOnTouchListener { view, motionEvent ->
             when (motionEvent.action) {

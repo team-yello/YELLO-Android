f5b5dd929ebd3fdd78683c704b5b8ed97b9db406
Sangho Kim <marchbreeze03@gmail.com>
2023-07-22 06:57:22 +0900

    [MOD] 버전 코드 5 업데이트

--- buildSrc/src/main/kotlin/Constants.kt
+++ buildSrc/src/main/kotlin/Constants.kt
@@ -3,6 +3,6 @@ object Constants {
     const val compileSdk = 33
     const val minSdk = 28
     const val targetSdk = 33
-    const val versionCode = 3
+    const val versionCode = 5
     const val versionName = "1.0"
 }

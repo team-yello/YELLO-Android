d40b98b4e79048ff0432c671ec654a0f8e4de223
pmj1459 <76741702+minju1459@users.noreply.github.com>
2023-07-22 15:10:56 +0900

    [MOD] 버전 코드 6 업데이트

--- buildSrc/src/main/kotlin/Constants.kt
+++ buildSrc/src/main/kotlin/Constants.kt
@@ -3,6 +3,6 @@ object Constants {
     const val compileSdk = 33
     const val minSdk = 28
     const val targetSdk = 33
-    const val versionCode = 5
+    const val versionCode = 6
     const val versionName = "1.0"
 }

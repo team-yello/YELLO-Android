f7d7182c3d5ae80db8217ec5ddad595427ff2184
Leekangmin <kkk5474096@naver.com>
2023-07-28 02:22:46 +0900

    [HOTFIX/#83] 토큰 자동 재발급 구현

--- data/src/main/java/com/example/data/remote/interceptor/AuthInterceptor.kt
+++ data/src/main/java/com/example/data/remote/interceptor/AuthInterceptor.kt
@@ -1,6 +1,7 @@
 package com.example.data.remote.interceptor
 
 import android.content.Context
+import com.example.data.model.response.BaseResponse
 import com.example.data.model.response.onboarding.ResponseAuthToken
 import com.example.domain.YelloDataStore
 import com.yello.data.BuildConfig.BASE_URL
@@ -8,6 +9,7 @@ import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.serialization.json.Json
 import okhttp3.Interceptor
 import okhttp3.Request
+import okhttp3.RequestBody.Companion.toRequestBody
 import okhttp3.Response
 import timber.log.Timber
 import javax.inject.Inject
@@ -27,27 +29,25 @@ class AuthInterceptor @Inject constructor(
         }
         val response = chain.proceed(authRequest)
 
-        when (response.code()) {
+        when (response.code) {
             CODE_TOKEN_EXPIRED -> {
                 try {
-                    val refreshTokenRequest = originalRequest.newBuilder().get()
+                    val refreshTokenRequest = originalRequest.newBuilder().post("".toRequestBody())
                         .url("$BASE_URL/api/v1/auth/token/issue")
-                        .addHeader(HEADER_ACCESS_TOKEN, dataStore.userToken)
-                        .addHeader(HEADER_REFRESH_TOKEN, dataStore.refreshToken)
+                        .addHeader(HEADER_ACCESS_TOKEN, "Bearer ${dataStore.userToken}")
+                        .addHeader(HEADER_REFRESH_TOKEN, "Bearer ${dataStore.refreshToken}")
                         .build()
                     val refreshTokenResponse = chain.proceed(refreshTokenRequest)
                     Timber.d("GET REFRESH TOKEN : $refreshTokenResponse")
 
                     if (refreshTokenResponse.isSuccessful) {
-                        val responseToken: ResponseAuthToken =
-                            json.decodeFromString(
-                                refreshTokenResponse.body()?.string()
-                                    ?: throw IllegalStateException("refreshTokenResponse is null $refreshTokenResponse"),
-                            )
+                        val responseToken = json.decodeFromString(
+                            refreshTokenResponse.body?.string().toString()
+                        ) as BaseResponse<ResponseAuthToken>
 
                         with(dataStore) {
-                            userToken = responseToken.accessToken
-                            refreshToken = responseToken.refreshToken
+                            userToken = responseToken.data?.accessToken ?: ""
+                            refreshToken = responseToken.data?.refreshToken ?: ""
                         }
                         refreshTokenResponse.close()
                         val newRequest = originalRequest.newAuthBuilder().build()
@@ -78,7 +78,7 @@ class AuthInterceptor @Inject constructor(
     companion object {
         private const val CODE_TOKEN_EXPIRED = 401
         private const val HEADER_AUTHORIZATION = "Authorization"
-        private const val HEADER_ACCESS_TOKEN = "accessToken"
-        private const val HEADER_REFRESH_TOKEN = "refreshToken"
+        private const val HEADER_ACCESS_TOKEN = "X-ACCESS-AUTH"
+        private const val HEADER_REFRESH_TOKEN = "X-REFRESH-AUTH"
     }
 }

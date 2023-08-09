15b3240acb96114d66c1ac77d5c5362d45abeabe
Sangho Kim <marchbreeze03@gmail.com>
2023-08-09 19:17:18 +0900

    [FEAT/#99] 둘러보기 서버통신 사전 구현

--- app/src/main/java/com/el/yello/presentation/main/look/LookFragment.kt
+++ app/src/main/java/com/el/yello/presentation/main/look/LookFragment.kt
@@ -2,19 +2,53 @@ package com.el.yello.presentation.main.look
 
 import android.os.Bundle
 import android.view.View
+import androidx.fragment.app.viewModels
 import com.el.yello.R
 import com.el.yello.databinding.FragmentLookBinding
+import com.el.yello.presentation.main.recommend.school.RecommendSchoolViewModel
 import com.el.yello.presentation.onboarding.fragment.addfriend.AddFriendAdapter
+import com.el.yello.util.context.yelloSnackbar
 import com.example.ui.base.BindingFragment
+import com.example.ui.view.UiState
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class LookFragment : BindingFragment<FragmentLookBinding>(R.layout.fragment_look) {
 
     private var _adapter: LookAdapter? = null
     private val adapter
         get() = requireNotNull(_adapter) { getString(R.string.adapter_not_initialized_error_msg) }
 
+    private val viewModel by viewModels<LookViewModel>()
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        initFirstList()
+        observeLookListDataState()
+    }
+
+    private fun initFirstList() {
+        viewModel.addLookListFromServer(page = 0)
+    }
+
+    // 둘러보기 목록 서버 통신 성공 시 어댑터에 리스트 추가
+    private fun observeLookListDataState() {
+        viewModel.getLookListState.observe(viewLifecycleOwner) { state ->
+            when (state) {
+                is UiState.Success -> {
+                    adapter.submitList(state.data?.data)
+                }
+
+                is UiState.Failure -> {
+                    yelloSnackbar(requireView(), getString(R.string.look_error_friend_list))
+                }
+
+                is UiState.Empty -> {}
+
+                is UiState.Loading -> {}
+            }
+        }
     }
 
     companion object {
---
+++ app/src/main/java/com/el/yello/presentation/main/look/LookViewModel.kt
@@ -0,0 +1,37 @@
+package com.el.yello.presentation.main.look
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.domain.entity.LookListModel
+import com.example.domain.repository.LookRepository
+import com.example.ui.view.UiState
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class LookViewModel @Inject constructor(
+    private val lookRepository: LookRepository
+) : ViewModel() {
+
+    private val _getLookListState = MutableLiveData<UiState<LookListModel?>>()
+    val getLookListState: LiveData<UiState<LookListModel?>> = _getLookListState
+
+    // 서버 통신 - 둘러보기 리스트 추가
+    fun addLookListFromServer(page: Int) {
+        viewModelScope.launch {
+            runCatching {
+                lookRepository.getLookList(
+                    page
+                )
+            }.onSuccess {
+                it ?: return@launch
+                _getLookListState.value = UiState.Success(it)
+            }.onFailure {
+                _getLookListState.value = UiState.Failure(it.message ?: "")
+            }
+        }
+    }
+}

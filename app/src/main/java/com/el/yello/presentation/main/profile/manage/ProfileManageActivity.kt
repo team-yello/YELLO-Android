9b8cf2ed94912cfab1df7b1a0f9b3b121c19b80f
Sangho Kim <marchbreeze03@gmail.com>
2023-08-13 17:53:55 +0900

    [UI/#123] 프로필 관리뷰 one, two 교체

--- app/src/main/java/com/el/yello/presentation/main/profile/manage/ProfileManageActivity.kt
+++ app/src/main/java/com/el/yello/presentation/main/profile/manage/ProfileManageActivity.kt
@@ -73,7 +73,7 @@ class ProfileManageActivity :
 
     private fun initQuitBtnListener() {
         binding.btnProfileManageQuit.setOnSingleClickListener {
-            Intent(this, ProfileQuitActivity::class.java).apply {
+            Intent(this, ProfileQuitTwoActivity::class.java).apply {
                 addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
                 startActivity(this)
             }

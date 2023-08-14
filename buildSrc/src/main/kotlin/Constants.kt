c2ef71395fb9b13e2427106e2f57c2b98f0111c2
Sangho Kim <marchbreeze03@gmail.com>
2023-08-15 00:03:49 +0900

    [FIX/#132] versionCode 13 업데이트

--- buildSrc/src/main/kotlin/Constants.kt
+++ buildSrc/src/main/kotlin/Constants.kt
@@ -3,6 +3,6 @@ object Constants {
     const val compileSdk = 33
     const val minSdk = 28
     const val targetSdk = 33
-    const val versionCode = 12
+    const val versionCode = 13
     const val versionName = "1.0"
 }

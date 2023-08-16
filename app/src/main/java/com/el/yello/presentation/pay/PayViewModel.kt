d457004ed05151a5a5653332506c47a964b5bd71
Sangho Kim <marchbreeze03@gmail.com>
2023-08-17 03:12:59 +0900

    [ADD/#136] 결제 API ViewModel 추가

--- app/src/main/java/com/el/yello/presentation/pay/PayViewModel.kt
+++ app/src/main/java/com/el/yello/presentation/pay/PayViewModel.kt
@@ -1,23 +1,36 @@
 package com.el.yello.presentation.pay
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.domain.entity.RequestPayModel
 import com.example.domain.entity.Response
+import com.example.domain.entity.ResponsePaySubsModel
+import com.example.domain.entity.vote.ResponsePayInAppModel
+import com.example.domain.repository.PayRepository
 import com.example.domain.repository.YelloRepository
 import com.example.ui.view.UiState
 import dagger.hilt.android.lifecycle.HiltViewModel
-import javax.inject.Inject
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.launch
+import javax.inject.Inject
 
 @HiltViewModel
 class PayViewModel @Inject constructor(
     private val repository: YelloRepository,
+    private val payRepository: PayRepository,
 ) : ViewModel() {
     private val _payCheck = MutableSharedFlow<UiState<Response>>()
     val payCheck: SharedFlow<UiState<Response>> = _payCheck.asSharedFlow()
+
+    private val _postSubsCheckState = MutableLiveData<UiState<ResponsePaySubsModel?>>()
+    val postSubsCheckState: LiveData<UiState<ResponsePaySubsModel?>> = _postSubsCheckState
+
+    private val _postInAppCheckState = MutableLiveData<UiState<ResponsePayInAppModel?>>()
+    val postInAppCheckState: LiveData<UiState<ResponsePayInAppModel?>> = _postInAppCheckState
 
     fun payCheck(index: Int) {
         _payCheck.tryEmit(UiState.Loading)
@@ -28,6 +41,36 @@ class PayViewModel @Inject constructor(
                 }.onFailure {
                     _payCheck.emit(UiState.Failure(""))
                 }
+        }
+    }
+
+    fun checkSubsToServer(request: RequestPayModel) {
+        viewModelScope.launch {
+            _postSubsCheckState.value = UiState.Loading
+            runCatching {
+                payRepository.postToCheckSubs(
+                    request
+                )
+            }.onSuccess {
+                _postSubsCheckState.value = UiState.Success(it)
+            }.onFailure {
+                _postSubsCheckState.value = UiState.Failure(it.message ?: "")
+            }
+        }
+    }
+
+    fun checkInAppToServer(request: RequestPayModel) {
+        viewModelScope.launch {
+            _postInAppCheckState.value = UiState.Loading
+            runCatching {
+                payRepository.postToCheckInApp(
+                    request
+                )
+            }.onSuccess {
+                _postInAppCheckState.value = UiState.Success(it)
+            }.onFailure {
+                _postInAppCheckState.value = UiState.Failure(it.message ?: "")
+            }
         }
     }
 }

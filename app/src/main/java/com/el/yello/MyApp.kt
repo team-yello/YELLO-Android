e8bc313465f82a3ce103dfdfa2e276a11357e70e
pmj1459 <76741702+minju1459@users.noreply.github.com>
2023-08-18 20:08:18 +0900

    [CHORE/#146] amplitude util 함수 추가

--- app/src/main/java/com/el/yello/MyApp.kt
+++ app/src/main/java/com/el/yello/MyApp.kt
@@ -1,10 +1,12 @@
 package com.el.yello
 
 import android.app.Application
+import com.amplitude.api.Amplitude
 import com.el.yello.BuildConfig.NATIVE_APP_KEY
 import com.el.yello.presentation.util.ResolutionMetrics
 import com.kakao.sdk.common.KakaoSdk
 import dagger.hilt.android.HiltAndroidApp
+import dagger.hilt.android.internal.Contexts.getApplication
 import timber.log.Timber
 import javax.inject.Inject
 
@@ -20,6 +22,9 @@ class MyApp : Application() {
         setUpFlipper()
         KakaoSdk.init(this, NATIVE_APP_KEY)
         resolutionMetrics = metrics
+
+        Amplitude.getInstance().initialize(this, "AMPLITUDE_KEY")
+            .enableForegroundTracking(this)
     }
 
     companion object {

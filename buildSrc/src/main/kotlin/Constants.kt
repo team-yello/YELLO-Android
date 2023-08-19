540ad5f78f15039317ad6804cd844ad5ae3adc07
Sangho Kim <marchbreeze03@gmail.com>
2023-08-19 16:27:24 +0900

    [FIX/#151] 버전 1(1.1) 변경

--- buildSrc/src/main/kotlin/Constants.kt
+++ buildSrc/src/main/kotlin/Constants.kt
@@ -3,6 +3,6 @@ object Constants {
     const val compileSdk = 33
     const val minSdk = 28
     const val targetSdk = 33
-    const val versionCode = 14
-    const val versionName = "1.0"
+    const val versionCode = 1
+    const val versionName = "1.1"
 }

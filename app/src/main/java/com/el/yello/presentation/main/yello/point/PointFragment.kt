6c2daaf455ea971ceb842210a4a91fe01b4df51e
Sangho Kim <marchbreeze03@gmail.com>
2023-08-21 03:20:06 +0900

    [ADD/#166] 투표종료 뷰 구독 여부 확인

--- app/src/main/java/com/el/yello/presentation/main/yello/point/PointFragment.kt
+++ app/src/main/java/com/el/yello/presentation/main/yello/point/PointFragment.kt
@@ -8,6 +8,7 @@ import com.el.yello.databinding.FragmentPointBinding
 import com.el.yello.presentation.main.yello.YelloViewModel
 import com.el.yello.presentation.main.yello.vote.VoteViewModel
 import com.example.ui.base.BindingFragment
+import com.example.ui.view.UiState
 import com.example.ui.view.setOnSingleClickListener
 
 class PointFragment : BindingFragment<FragmentPointBinding>(R.layout.fragment_point) {
@@ -19,12 +20,37 @@ class PointFragment : BindingFragment<FragmentPointBinding>(R.layout.fragment_po
         binding.vm = voteViewModel
 
         setConfirmBtnClickListener()
+        observeCheckIsSubscribed()
+        yelloViewModel.getPurchaseInfoFromServer()
     }
 
     private fun setConfirmBtnClickListener() {
         binding.btnPointConfirm.setOnSingleClickListener {
             yelloViewModel.getVoteState()
             requireActivity().finish()
+        }
+    }
+
+    // 구독 여부 확인
+    private fun observeCheckIsSubscribed() {
+        yelloViewModel.getPurchaseInfoState.observe(viewLifecycleOwner) { state ->
+            when (state) {
+                is UiState.Success -> {
+                    if (state.data?.isSubscribe == true) {
+                        binding.tvPointPointLabel.visibility = View.VISIBLE
+                    } else {
+                        binding.tvPointPointLabel.visibility = View.GONE
+                    }
+                }
+
+                is UiState.Failure -> {
+                    binding.tvPointPointLabel.visibility = View.GONE
+                }
+
+                is UiState.Loading -> {}
+
+                is UiState.Empty -> {}
+            }
         }
     }
 

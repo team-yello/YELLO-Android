a89456835cb3c7f129b817df3c85a4ceaa3c766e
pmj1459 <76741702+minju1459@users.noreply.github.com>
2023-09-14 02:06:20 +0900

    [FIX/#257] group item string -> int

--- app/src/main/java/com/el/yello/presentation/onboarding/fragment/highschoolinfo/group/GroupDialogAdapter.kt
+++ app/src/main/java/com/el/yello/presentation/onboarding/fragment/highschoolinfo/group/GroupDialogAdapter.kt
@@ -33,7 +33,7 @@ class GroupDialogAdapter(
     ) :
         RecyclerView.ViewHolder(binding.root) {
         fun setGroup(group: Int) {
-            binding.group = group.toString()
+            binding.group = group
             binding.root.setOnSingleClickListener {
                 storeGroup(group)
                 binding.tvItemGroup.setBackgroundResource(R.drawable.shape_grayscales_800_fill_100_rect)

9738c4836c0045a4c3e86f483fcfba80a7bc7bee
Sangho Kim <marchbreeze03@gmail.com>
2023-09-18 16:49:52 +0900

    [CHORE/#264] version code 27 업데이트

--- buildSrc/src/main/kotlin/Constants.kt
+++ buildSrc/src/main/kotlin/Constants.kt
@@ -3,6 +3,6 @@ object Constants {
     const val compileSdk = 33
     const val minSdk = 28
     const val targetSdk = 33
-    const val versionCode = 26
+    const val versionCode = 27
     const val versionName = "1.2"
 }

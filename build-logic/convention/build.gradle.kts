77aec9010efec4c8816438de38ed3bb964a5c51c
Leekangmin <kkk5474096@naver.com>
2023-10-06 18:31:34 +0900

    [CHORE/#292] build-logic & convention module 생성

---
+++ build-logic/convention/build.gradle.kts
@@ -0,0 +1,13 @@
+plugins {
+    `kotlin-dsl`
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
+dependencies {
+    compileOnly(libs.android.build)
+    compileOnly(libs.kotlin.gradle)
+}

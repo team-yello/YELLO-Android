17eb7bac500b13c4e3ff67dc8e1d0ecdd42fe0cf
pmj1459 <76741702+minju1459@users.noreply.github.com>
2023-10-08 14:51:19 +0900

    [DEL/#288] Log 삭제

--- app/src/main/java/com/el/yello/presentation/tutorial/TutorialDActivity.kt
+++ app/src/main/java/com/el/yello/presentation/tutorial/TutorialDActivity.kt
@@ -33,14 +33,11 @@ class TutorialDActivity : BindingActivity<ActivityTutorialDBinding>(R.layout.act
         val isCodeTextEmpty = intent.getBooleanExtra(OnBoardingActivity.EXTRA_CODE_TEXT_EMPTY, false)
         val isFromOnBoarding = intent.getBooleanExtra(TutorialAActivity.EXTRA_FROM_ONBOARDING, false)
         binding.root.setOnSingleClickListener {
-            Log.d("minju",isCodeTextEmpty.toString())
             if (isFromOnBoarding) {
                 if (isCodeTextEmpty) {
-                    Log.d("minju2",isCodeTextEmpty.toString())
                     val intent = Intent(this@TutorialDActivity, TutorialEndActivity::class.java)
                     startActivity(intent)
                 } else {
-                    Log.d("minju3",isCodeTextEmpty.toString())
                     val intent = Intent(this@TutorialDActivity, TutorialEndPlusActivity::class.java)
                     startActivity(intent)
                 }

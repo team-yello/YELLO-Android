c960795e99b3a7c0de3efa7dc33aecc2de9dde50
Leekangmin <kkk5474096@naver.com>
2023-10-12 18:39:30 +0900

    [CHORE/#292] build-logic plugins

--- build-logic/convention/build.gradle.kts
+++ build-logic/convention/build.gradle.kts
@@ -11,3 +11,20 @@ dependencies {
     compileOnly(libs.android.build)
     compileOnly(libs.kotlin.gradle)
 }
+
+gradlePlugin {
+    plugins {
+        register("androidApplication") {
+            id = "yello.android.application"
+            implementationClass = "plugins.AndroidApplicationPlugin"
+        }
+        register("androidHilt") {
+            id = "yello.android.hilt"
+            implementationClass = "plugins.AndroidHiltPlugin"
+        }
+        register("androidKotlin") {
+            id = "yello.android.kotlin"
+            implementationClass = "plugins.AndroidKotlinPlugin"
+        }
+    }
+}

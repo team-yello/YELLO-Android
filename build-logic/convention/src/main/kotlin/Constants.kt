e25f68f09d9e09f30da69b1742f8bbbc537471cc
Leekangmin <kkk5474096@naver.com>
2023-10-12 18:32:11 +0900

    [CHORE/#292] Constants

---
+++ build-logic/convention/src/main/kotlin/Constants.kt
@@ -0,0 +1,12 @@
+import org.gradle.api.JavaVersion
+
+object Constants {
+    const val packageName = "com.el.yello"
+    const val compileSdk = 33
+    const val minSdk = 28
+    const val targetSdk = 33
+    const val versionCode = 33
+    const val versionName = "1.3"
+    val javaVersion = JavaVersion.VERSION_17
+    const val jvmVersion = "17"
+}

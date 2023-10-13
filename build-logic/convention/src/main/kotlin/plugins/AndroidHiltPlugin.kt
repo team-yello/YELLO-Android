19b0d87555d27de3c55fd729b1bf192a6fdfda9f
Leekangmin <kkk5474096@naver.com>
2023-10-13 18:19:57 +0900

    [CHORE/#292] AndroidHiltPlugin 구현 완료

--- build-logic/convention/src/main/kotlin/plugins/AndroidHiltPlugin.kt
+++ build-logic/convention/src/main/kotlin/plugins/AndroidHiltPlugin.kt
@@ -6,7 +6,7 @@ import org.gradle.api.Project
 /**
  * Hilt 를 적용할 모듈에 사용할 플러그인
  *
- * plugin id : [yello.android.hilt]
+ * plugin id : [yello.android.androidHilt]
  */
 class AndroidHiltPlugin: Plugin<Project> {
     override fun apply(target: Project) {

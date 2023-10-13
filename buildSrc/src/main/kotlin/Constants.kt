e9d613ecc63f901d4448ef88dee166a73afe6a01
Leekangmin <kkk5474096@naver.com>
2023-10-13 18:16:42 +0900

    [CHORE/#292] buildSrc 제거

--- buildSrc/src/main/kotlin/Constants.kt
+++
@@ -1,8 +0,0 @@
-object Constants {
-    const val packageName = "com.el.yello"
-    const val compileSdk = 33
-    const val minSdk = 28
-    const val targetSdk = 33
-    const val versionCode = 31
-    const val versionName = "1.3"
-}

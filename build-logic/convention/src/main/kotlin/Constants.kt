9bbb68aa56693ed7388717418366099dc059be01
Leekangmin <kkk5474096@naver.com>
2023-10-13 18:17:19 +0900

    [CHORE/#292] Constants 수정

--- build-logic/convention/src/main/kotlin/Constants.kt
+++ build-logic/convention/src/main/kotlin/Constants.kt
@@ -1,5 +1,3 @@
-import org.gradle.api.JavaVersion
-
 object Constants {
     const val packageName = "com.el.yello"
     const val compileSdk = 33
@@ -7,6 +5,5 @@ object Constants {
     const val targetSdk = 33
     const val versionCode = 33
     const val versionName = "1.3"
-    val javaVersion = JavaVersion.VERSION_17
     const val jvmVersion = "17"
 }

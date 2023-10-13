656dcae38f168a8cbc2b5c9bffe8f659148a961c
Leekangmin <kkk5474096@naver.com>
2023-10-13 18:19:20 +0900

    [CHORE/#292] AndroidKotlinPlugin 구현 완료

--- build-logic/convention/src/main/kotlin/plugins/AndroidKotlinPlugin.kt
+++ build-logic/convention/src/main/kotlin/plugins/AndroidKotlinPlugin.kt
@@ -1,43 +1,21 @@
 package plugins
 
-import com.android.build.gradle.BaseExtension
-import ext.coreLibraryDesugaring
 import ext.getBundle
 import ext.getVersionCatalog
 import ext.implementation
-import org.gradle.api.JavaVersion
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.api.artifacts.VersionCatalogsExtension
-import org.gradle.api.plugins.ExtensionAware
-import org.gradle.kotlin.dsl.configure
 import org.gradle.kotlin.dsl.dependencies
-import org.gradle.kotlin.dsl.getByType
-import org.jetbrains.kotlin.gradle.dsl.KotlinJvmOptions
 
+/**
+ * kotlin 를 적용할 모듈에 사용할 플러그인
+ *
+ * plugin id : [yello.android.kotlin]
+ */
 class AndroidKotlinPlugin : Plugin<Project> {
     override fun apply(target: Project) = with(target) {
         with(pluginManager) {
             apply("kotlin-android")
-        }
-
-        extensions.getByType<BaseExtension>().apply {
-            setCompileSdkVersion(libs.findVersion("compileSdk").get().requiredVersion.toInt())
-
-            defaultConfig {
-                minSdk = libs.findVersion("minSdk").get().requiredVersion.toInt()
-                targetSdk = libs.findVersion("targetSdk").get().requiredVersion.toInt()
-            }
-
-            compileOptions {
-                isCoreLibraryDesugaringEnabled = true
-                sourceCompatibility = JavaVersion.VERSION_17
-                targetCompatibility = JavaVersion.VERSION_17
-            }
-
-            (this as ExtensionAware).configure<KotlinJvmOptions> {
-                jvmTarget = "17"
-            }
         }
 
         val libs = extensions.getVersionCatalog()

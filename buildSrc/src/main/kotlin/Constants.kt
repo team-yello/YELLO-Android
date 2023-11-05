a477e7924ef19783bd24c411913465911d3c649a
Sangho Kim <marchbreeze03@gmail.com>
2023-11-05 21:50:03 +0900

    [CHORE/#321] version code 34 업데이트

--- buildSrc/src/main/kotlin/Constants.kt
+++ buildSrc/src/main/kotlin/Constants.kt
@@ -3,6 +3,6 @@ object Constants {
     const val compileSdk = 33
     const val minSdk = 28
     const val targetSdk = 33
-    const val versionCode = 33
+    const val versionCode = 34
     const val versionName = "1.4"
 }

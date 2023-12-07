f392f68c814f2143aafaaab6010e919fef8d0f45
pmj1459 <76741702+minju1459@users.noreply.github.com>
2023-12-07 23:35:14 +0900

    [CHORE/#337] version code 38

--- build-logic/convention/src/main/kotlin/Constants.kt
+++ build-logic/convention/src/main/kotlin/Constants.kt
@@ -3,7 +3,7 @@ object Constants {
     const val compileSdk = 33
     const val minSdk = 28
     const val targetSdk = 33
-    const val versionCode = 37
+    const val versionCode = 38
     const val versionName = "1.4"
     const val jvmVersion = "17"
 }

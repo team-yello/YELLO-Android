4a960f86eb383d4bcbcc67ba536994cd8b0658d8
pmj1459 <76741702+minju1459@users.noreply.github.com>
2023-12-11 05:02:36 +0900

    [FEAT/#339] 이름 확인 dilaog 분기 처리

--- app/src/main/java/com/el/yello/presentation/onboarding/fragment/checkName/CheckNameDialog.kt
+++ app/src/main/java/com/el/yello/presentation/onboarding/fragment/checkName/CheckNameDialog.kt
@@ -1,17 +1,21 @@
-package com.el.yello.presentation.onboarding.fragment.EditName
+package com.el.yello.presentation.onboarding.fragment.checkName
 
+import android.content.Intent
 import android.content.res.Resources
 import android.os.Bundle
 import android.view.View
 import android.view.WindowManager
 import androidx.fragment.app.activityViewModels
 import com.el.yello.R
-import com.el.yello.databinding.FragmentNameEditDialogBinding
+import com.el.yello.databinding.FragmentDialogCheckNameBinding
 import com.el.yello.presentation.main.profile.ProfileViewModel
+import com.el.yello.presentation.onboarding.activity.EditNameActivity
+import com.el.yello.presentation.onboarding.activity.OnBoardingActivity
 import com.example.ui.base.BindingDialogFragment
+import com.example.ui.view.setOnSingleClickListener
 
-class EditNameDialog :
-    BindingDialogFragment<FragmentNameEditDialogBinding>(R.layout.fragment_name_edit_dialog) {
+class CheckNameDialog :
+    BindingDialogFragment<FragmentDialogCheckNameBinding>(R.layout.fragment_dialog_check_name) {
 
     private val viewModel by activityViewModels<ProfileViewModel>()
 
@@ -37,5 +41,17 @@ class EditNameDialog :
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        initEditBtnListener()
+    }
+
+    private fun initEditBtnListener() {
+        binding.btnNameEditDialogYes.setOnSingleClickListener {
+            val intent = Intent(requireContext(), OnBoardingActivity::class.java)
+            startActivity(intent)
+        }
+        binding.btnNameEditDialogNo.setOnSingleClickListener {
+            val intent = Intent(requireContext(), EditNameActivity::class.java)
+            startActivity(intent)
+        }
     }
 }

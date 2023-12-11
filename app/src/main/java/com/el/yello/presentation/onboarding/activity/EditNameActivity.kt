b6da26a46e86ccce0f6fe127dbdc77eb59e08b8a
pmj1459 <76741702+minju1459@users.noreply.github.com>
2023-12-11 22:24:53 +0900

    [FEAT/#339] 이름 수정뷰 뒤로 가기 막기

--- app/src/main/java/com/el/yello/presentation/onboarding/activity/EditNameActivity.kt
+++ app/src/main/java/com/el/yello/presentation/onboarding/activity/EditNameActivity.kt
@@ -2,10 +2,13 @@ package com.el.yello.presentation.onboarding.activity
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import androidx.activity.OnBackPressedCallback
 import androidx.activity.viewModels
 import com.el.yello.R
 import com.el.yello.databinding.ActivityNameEditBinding
 import com.example.ui.base.BindingActivity
+import com.example.ui.context.toast
 import com.example.ui.view.setOnSingleClickListener
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -15,16 +18,31 @@ class EditNameActivity :
 
     private val viewModel by viewModels<OnBoardingViewModel>()
 
+    private var backPressedTime: Long = 0
+
+    private val onBackPressedCallback = object : OnBackPressedCallback(true) {
+        override fun handleOnBackPressed() {
+            if (System.currentTimeMillis() - backPressedTime >= BACK_PRESSED_INTERVAL) {
+                backPressedTime = System.currentTimeMillis()
+                toast(getString(R.string.main_toast_back_pressed))
+            } else {
+                finish()
+            }
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding.vm = viewModel
 
         setConfirmBtnClickListener()
         setDeleteBtnClickListener()
+        this.onBackPressedDispatcher.addCallback(this, onBackPressedCallback)
     }
 
     private fun setConfirmBtnClickListener() {
         getIntentExtraData()
+        Log.d("mmm", viewModel.name)
         binding.btnNameNext.setOnSingleClickListener {
             val intent = Intent(this, OnBoardingActivity::class.java).apply {
                 intent.putExtra("EXTRA_KAKAO_ID", viewModel.kakaoId.toLong())
@@ -35,6 +53,7 @@ class EditNameActivity :
             }
             startActivity(intent)
         }
+        Log.d("mmm2", viewModel.name)
     }
 
     private fun setDeleteBtnClickListener() {
@@ -54,5 +73,9 @@ class EditNameActivity :
                 viewModel.profileImg = bundle.getString("EXTRA_PROFILE_IMAGE", "")
             }
         }
+    }
+
+    companion object {
+        private const val BACK_PRESSED_INTERVAL = 2000
     }
 }

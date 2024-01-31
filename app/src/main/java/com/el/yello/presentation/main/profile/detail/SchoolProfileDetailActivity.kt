afda80b109f53fab531671ef7463b0307d2c26a4
Sangho Kim <marchbreeze03@gmail.com>
2024-01-31 18:37:31 +0900

    [ADD/#357] 중고딩 조회뷰 액티비티 설정

--- app/src/main/java/com/el/yello/presentation/main/profile/detail/SchoolProfileDetailActivity.kt
+++ app/src/main/java/com/el/yello/presentation/main/profile/detail/SchoolProfileDetailActivity.kt
@@ -2,10 +2,21 @@ package com.el.yello.presentation.main.profile.detail
 
 import android.os.Bundle
 import androidx.activity.viewModels
+import androidx.lifecycle.flowWithLifecycle
+import androidx.lifecycle.lifecycleScope
 import com.el.yello.R
 import com.el.yello.databinding.ActivityProfileSchoolDetailBinding
+import com.el.yello.presentation.main.profile.mod.UnivProfileModActivity
+import com.el.yello.util.Utils.setImageOrBasicThumbnail
+import com.el.yello.util.context.yelloSnackbar
+import com.example.ui.activity.navigateTo
 import com.example.ui.base.BindingActivity
+import com.example.ui.context.toast
+import com.example.ui.view.UiState
+import com.example.ui.view.setOnSingleClickListener
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 
 @AndroidEntryPoint
 class SchoolProfileDetailActivity :
@@ -16,5 +27,86 @@ class SchoolProfileDetailActivity :
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        binding.vm = viewModel
+        initProfileModBtnListener()
+        initChangeThumbnailBtnListener()
+        initBackBtnListener()
+        observeUserDataState()
+        observeKakaoDataResult()
+        observeModProfileState()
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        viewModel.getUserDataFromServer()
+    }
+
+    private fun initProfileModBtnListener() {
+        binding.btnModSchool.setOnSingleClickListener {
+            this.navigateTo<UnivProfileModActivity>()
+            viewModel.resetViewModelState()
+        }
+        binding.btnModGrade.setOnSingleClickListener {
+            this.navigateTo<UnivProfileModActivity>()
+            viewModel.resetViewModelState()
+        }
+        binding.btnModClassroom.setOnSingleClickListener {
+            this.navigateTo<UnivProfileModActivity>()
+            viewModel.resetViewModelState()
+        }
+    }
+
+    private fun initChangeThumbnailBtnListener() {
+        binding.btnChangeKakaoImage.setOnSingleClickListener {
+            viewModel.getUserInfoFromKakao()
+        }
+    }
+
+    private fun initBackBtnListener() {
+        binding.btnProfileDetailBack.setOnSingleClickListener { finish() }
+    }
+
+    private fun observeUserDataState() {
+        viewModel.getUserDataState.flowWithLifecycle(lifecycle).onEach { state ->
+            when (state) {
+                is UiState.Success -> {
+                    binding.ivProfileDetailThumbnail.setImageOrBasicThumbnail(state.data)
+                }
+
+                is UiState.Failure -> {
+                    toast(getString(R.string.profile_error_user_data))
+                }
+
+                is UiState.Empty -> return@onEach
+
+                is UiState.Loading -> return@onEach
+            }
+        }.launchIn(lifecycleScope)
+    }
+
+    private fun observeKakaoDataResult() {
+        viewModel.getKakaoInfoResult.flowWithLifecycle(lifecycle).onEach { result ->
+            if (!result) yelloSnackbar(binding.root, getString(R.string.msg_error))
+        }.launchIn(lifecycleScope)
+    }
+
+    private fun observeModProfileState() {
+        viewModel.postToModProfileState.flowWithLifecycle(lifecycle).onEach { state ->
+            when (state) {
+                is UiState.Success -> {
+                    binding.ivProfileDetailThumbnail.setImageOrBasicThumbnail(state.data)
+                    toast(getString(R.string.profile_detail_image_change))
+                }
+
+                is UiState.Failure -> {
+                    toast(getString(R.string.sign_in_error_connection))
+                }
+
+                is UiState.Empty -> return@onEach
+
+                is UiState.Loading -> return@onEach
+            }
+        }.launchIn(lifecycleScope)
     }
 }

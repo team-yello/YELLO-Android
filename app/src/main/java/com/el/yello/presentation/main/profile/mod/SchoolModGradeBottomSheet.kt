e9d87948669c33a4ac8a19b62d724cca9177d13c
Sangho Kim <marchbreeze03@gmail.com>
2024-02-01 02:59:10 +0900

    [FEAT/#357] 중고딩 학년 바텀시트 설정

---
+++ app/src/main/java/com/el/yello/presentation/main/profile/mod/SchoolModGradeBottomSheet.kt
@@ -0,0 +1,31 @@
+package com.el.yello.presentation.main.profile.mod
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.activityViewModels
+import com.el.yello.R
+import com.el.yello.databinding.FragmentModGradeBottomSheetBinding
+import com.example.ui.base.BindingBottomSheetDialog
+import com.example.ui.view.setOnSingleClickListener
+
+class SchoolModGradeBottomSheet :
+    BindingBottomSheetDialog<FragmentModGradeBottomSheetBinding>(R.layout.fragment_mod_grade_bottom_sheet) {
+
+    private val viewModel by activityViewModels<SchoolProfileModViewModel>()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initGradeBtnListener()
+    }
+
+    private fun initGradeBtnListener() {
+        binding.tvItemGradeOne.setOnSingleClickListener { setGrade(1) }
+        binding.tvItemGradeTwo.setOnSingleClickListener { setGrade(2) }
+        binding.tvItemGradeThree.setOnSingleClickListener { setGrade(3) }
+    }
+
+    private fun setGrade(grade: Int) {
+        viewModel.grade.value = grade.toString()
+    }
+}

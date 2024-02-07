eb0e6ea3977e2a9f6b6eb3c95969c27e9981f4de
b1urrrr <20190993@sungshin.ac.kr>
2024-02-07 23:07:22 +0900

    [FEAT/#374] 이벤트 참여 서버통신 연결

--- app/src/main/java/com/el/yello/presentation/event/reward/RewardDialog.kt
+++ app/src/main/java/com/el/yello/presentation/event/reward/RewardDialog.kt
@@ -3,18 +3,27 @@ package com.el.yello.presentation.event.reward
 import android.os.Bundle
 import android.view.View
 import android.view.WindowManager
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.flowWithLifecycle
+import androidx.lifecycle.lifecycleScope
+import coil.load
 import com.el.yello.R
 import com.el.yello.databinding.FragmentRewardDialogBinding
 import com.example.ui.base.BindingDialogFragment
+import com.example.ui.view.UiState
 import com.example.ui.view.setOnSingleClickListener
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 
 class RewardDialog :
     BindingDialogFragment<FragmentRewardDialogBinding>(R.layout.fragment_reward_dialog) {
+    private val viewModel by viewModels<RewardViewModel>()
 
     override fun onStart() {
         super.onStart()
 
         initContainerLayoutTransparent()
+        setupPostEventState()
     }
 
     private fun initContainerLayoutTransparent() {
@@ -37,6 +46,22 @@ class RewardDialog :
         binding.tvRewardConfirm.setOnSingleClickListener {
             dismiss()
         }
+    }
+
+    private fun setupPostEventState() {
+        viewModel.postEventState.flowWithLifecycle(viewLifecycleOwner.lifecycle)
+            .onEach { state ->
+                when (state) {
+                    is UiState.Success -> {
+                        binding.tvRewardTitle.text = state.data.title
+                        binding.ivReward.load(state.data.imageUrl)
+                    }
+
+                    is UiState.Failure -> dismiss()
+                    is UiState.Empty -> return@onEach
+                    is UiState.Loading -> return@onEach
+                }
+            }.launchIn(viewLifecycleOwner.lifecycleScope)
     }
 
     companion object {
---
+++ app/src/main/java/com/el/yello/presentation/event/reward/RewardViewModel.kt
@@ -0,0 +1,46 @@
+package com.el.yello.presentation.event.reward
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.domain.entity.event.EventResult
+import com.example.domain.repository.EventRepository
+import com.example.ui.view.UiState
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import retrofit2.HttpException
+import javax.inject.Inject
+
+@HiltViewModel
+class RewardViewModel @Inject constructor(
+    private val eventRepository: EventRepository,
+) : ViewModel() {
+    private val idempotencyKey = ""
+
+    private val _postEventState = MutableStateFlow<UiState<EventResult>>(UiState.Loading)
+    val postEventState: StateFlow<UiState<EventResult>> get() = _postEventState
+
+    init {
+        postEvent()
+    }
+
+    private fun postEvent() {
+        viewModelScope.launch {
+            eventRepository.postEvent(idempotencyKey)
+                .onSuccess { eventResult ->
+                    if (eventResult == null) {
+                        _postEventState.value = UiState.Empty
+                        return@onSuccess
+                    }
+
+                    _postEventState.value = UiState.Success(eventResult)
+                }
+                .onFailure { t ->
+                    if (t is HttpException) {
+                        _postEventState.value = UiState.Failure(t.code().toString())
+                    }
+                }
+        }
+    }
+}

e6a21bf8f31296c03f8ba9172c33cf19629cc34f
Sangho Kim <marchbreeze03@gmail.com>
2024-02-08 14:02:41 +0900

    [CHORE/#381] version name 2.0

--- build-logic/convention/src/main/kotlin/Constants.kt
+++ build-logic/convention/src/main/kotlin/Constants.kt
@@ -3,7 +3,7 @@ object Constants {
     const val compileSdk = 33
     const val minSdk = 28
     const val targetSdk = 33
-    const val versionCode = 39
-    const val versionName = "1.4"
+    const val versionCode = 40
+    const val versionName = "2.0"
     const val jvmVersion = "17"
 }

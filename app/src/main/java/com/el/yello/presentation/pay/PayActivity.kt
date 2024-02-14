03b236b4b75a33e0119505d9721b9d19f9dd6a7d
b1urrrr <20190993@sungshin.ac.kr>
2024-02-15 00:10:32 +0900

    [DEL/#254] unused toast import 제거

--- app/src/main/java/com/el/yello/presentation/pay/PayActivity.kt
+++ app/src/main/java/com/el/yello/presentation/pay/PayActivity.kt
@@ -20,7 +20,6 @@ import com.el.yello.util.amplitude.AmplitudeUtils
 import com.el.yello.util.context.yelloSnackbar
 import com.example.data.model.request.pay.toRequestPayModel
 import com.example.ui.base.BindingActivity
-import com.example.ui.context.toast
 import com.example.ui.view.UiState
 import com.example.ui.view.setOnSingleClickListener
 import dagger.hilt.android.AndroidEntryPoint

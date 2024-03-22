f4a042a819e764fcc1ca817eca9586c72fe1c933
Sangho Kim <marchbreeze03@gmail.com>
2024-03-22 09:33:24 +0900

    [FIX/#431] 공지 amplitude 수정

--- app/src/main/java/com/el/yello/presentation/main/dialog/notice/NoticeViewModel.kt
+++ app/src/main/java/com/el/yello/presentation/main/dialog/notice/NoticeViewModel.kt
@@ -1,6 +1,7 @@
 package com.el.yello.presentation.main.dialog.notice
 
 import androidx.lifecycle.ViewModel
+import com.el.yello.util.manager.AmplitudeManager
 import com.example.domain.repository.NoticeRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -19,6 +20,13 @@ class NoticeViewModel @Inject constructor(
     }
 
     fun setDisabledNotice(url: String) {
-        if (isNoticeDisabled.value) noticeRepository.setDisabledNoticeUrl(url)
+        if (isNoticeDisabled.value) {
+            noticeRepository.setDisabledNoticeUrl(url)
+            AmplitudeManager.trackEventWithProperties(EVENT_CLICK_POP_UP_NO)
+        }
+    }
+
+    companion object {
+        private const val EVENT_CLICK_POP_UP_NO = "click_notice_popup_no"
     }
 }

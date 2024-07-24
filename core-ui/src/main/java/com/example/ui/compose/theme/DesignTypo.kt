0546e5174457a6008a8d242f830a66429d1f17cc
Leekangmin <kkk5474096@naver.com>
2024-07-24 16:32:54 +0900

    [CHORE/#455] Compose Font 설정

---
+++ core-ui/src/main/java/com/example/ui/compose/theme/DesignTypo.kt
@@ -0,0 +1,13 @@
+package com.example.ui.compose.theme
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import com.yello.ui.R
+
+val PretendardFontFamily = FontFamily(
+    Font(R.font.font_pretendard_regular, FontWeight.Normal),
+    Font(R.font.font_pretendard_medium, FontWeight.Medium),
+    Font(R.font.font_pretendard_bold, FontWeight.Bold),
+    Font(R.font.font_pretendard_semibold, FontWeight.SemiBold)
+)

075474ca953933e0ecb373017e032f1cb5f3316a
Sangho Kim <marchbreeze03@gmail.com>
2024-08-07 02:20:09 +0900

    [CHORE/#464] version code 52 변경

--- build-logic/convention/src/main/kotlin/Constants.kt
+++ build-logic/convention/src/main/kotlin/Constants.kt
@@ -3,7 +3,7 @@ object Constants {
     const val compileSdk = 34
     const val minSdk = 28
     const val targetSdk = 34
-    const val versionCode = 51
+    const val versionCode = 52
     const val versionName = "2.2"
     const val jvmVersion = "18"
 }

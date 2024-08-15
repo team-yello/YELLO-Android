cb0f88a644b66509c578424e1594209c07667960
b1urrrr <20190993@sungshin.ac.kr>
2024-08-16 00:57:22 +0900

    [MOD/#466] 버전명 2.3 버전 코드 53으로 변경

--- build-logic/convention/src/main/kotlin/Constants.kt
+++ build-logic/convention/src/main/kotlin/Constants.kt
@@ -3,7 +3,7 @@ object Constants {
     const val compileSdk = 34
     const val minSdk = 28
     const val targetSdk = 34
-    const val versionCode = 52
-    const val versionName = "2.2"
+    const val versionCode = 53
+    const val versionName = "2.3"
     const val jvmVersion = "18"
 }

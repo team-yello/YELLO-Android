638e6c5898f4b2f616ca8a83afbaded509ffd6d4
Sangho Kim <marchbreeze03@gmail.com>
2025-02-17 23:55:16 +0900

    [UI/#469] 공사중 뷰 UI 설정

---
+++ app/src/main/java/com/el/yello/presentation/repairing/RepairingActivity.kt
@@ -0,0 +1,11 @@
+package com.el.yello.presentation.repairing
+
+import com.el.yello.R
+import com.el.yello.databinding.ActivityPayBinding
+import com.example.ui.base.BindingActivity
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class RepairingActivity : BindingActivity<ActivityPayBinding>(R.layout.activity_pay) {
+
+}

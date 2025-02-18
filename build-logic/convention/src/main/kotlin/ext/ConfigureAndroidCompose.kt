9115c00b705d98690cedb123157d4414688605c5
Sangho Kim <marchbreeze03@gmail.com>
2025-02-18 15:51:27 +0900

    [CHORE/#471] kotlinCompiler 버전 업데이트

--- build-logic/convention/src/main/kotlin/ext/ConfigureAndroidCompose.kt
+++ build-logic/convention/src/main/kotlin/ext/ConfigureAndroidCompose.kt
@@ -15,7 +15,7 @@ internal fun Project.configureAndroidCompose(
         }
 
         composeOptions {
-            kotlinCompilerExtensionVersion = "1.5.11"
+            kotlinCompilerExtensionVersion = "1.5.14"
         }
         val libs = extensions.getVersionCatalog()
 

a03f9d2c8910a17239a9afe7dd436396e8ba9e27
Leekangmin <kkk5474096@naver.com>
2025-07-01 15:15:04 +0900

    [REFACTOR/#473] MVI Orbit 셋팅

--- build-logic/convention/src/main/kotlin/ext/ConfigureAndroidCompose.kt
+++ build-logic/convention/src/main/kotlin/ext/ConfigureAndroidCompose.kt
@@ -27,6 +27,10 @@ internal fun Project.configureAndroidCompose(
             debugImplementation(libs.getLibrary("compose-bom-debug-ui-tooling"))
             androidTestImplementation(libs.getLibrary("compose-bom-debug-ui-test-manifest"))
             implementation(libs.getBundle("compose"))
+
+            // orbit 관련 의존성
+            implementation(libs.getBundle("orbit"))
+            testImplementation(libs.getLibrary("orbit.test"))
         }
     }
 }

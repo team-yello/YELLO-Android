199c589156c6c48f911626f25eb1cc7a72c5f6fe
Leekangmin <kkk5474096@naver.com>
2025-07-07 10:23:11 +0900

    [REFACTOR/#477] ProfileQuitTwoActivity 리팩토링 완료

--- app/src/main/java/com/el/yello/presentation/setting/ProfileQuitTwoActivity.kt
+++ app/src/main/java/com/el/yello/presentation/setting/ProfileQuitTwoActivity.kt
@@ -2,43 +2,28 @@ package com.el.yello.presentation.setting
 
 import android.content.Intent
 import android.os.Bundle
-import com.el.yello.R
-import com.el.yello.databinding.ActivityProfileQuitTwoBinding
-import com.el.yello.util.manager.AmplitudeManager
-import com.example.ui.base.BindingActivity
-import com.example.ui.extension.setOnSingleClickListener
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import com.example.ui.compose.theme.YelloTheme
 import dagger.hilt.android.AndroidEntryPoint
-import org.json.JSONObject
 
 @AndroidEntryPoint
-class ProfileQuitTwoActivity :
-    BindingActivity<ActivityProfileQuitTwoBinding>(R.layout.activity_profile_quit_two) {
+class ProfileQuitTwoActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        initBackBtnListener()
-        initQuitBtnListener()
-    }
-
-    private fun initBackBtnListener() {
-        binding.btnProfileQuitBack.setOnSingleClickListener { finish() }
-    }
-
-    private fun initQuitBtnListener() {
-        binding.btnProfileQuitForSure.setOnSingleClickListener {
-            AmplitudeManager.trackEventWithProperties(
-                EVENT_CLICK_PROFILE_WITHDRAWAL,
-                JSONObject().put(NAME_WITHDRAWAL_BUTTON, VALUE_WITHDRAWAL_THREE),
-            )
-            Intent(this, ProfileQuitReasonActivity::class.java).apply {
-                addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
-                startActivity(this)
+        setContent {
+            YelloTheme {
+                ProfileQuitTwoRoute(
+                    navigateBack = { finish() },
+                    navigateToProfileQuitReason = {
+                        Intent(this, ProfileQuitReasonActivity::class.java).apply {
+                            addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+                            startActivity(this)
+                        }
+                    }
+                )
             }
         }
-    }
-    companion object {
-        private const val EVENT_CLICK_PROFILE_WITHDRAWAL = "click_profile_withdrawal"
-        private const val NAME_WITHDRAWAL_BUTTON = "withdrawal_button"
-        private const val VALUE_WITHDRAWAL_THREE = "withdrawal3"
     }
 }

680d9fc9ff37ceaccc9df8db20a28a53209ba5f1
Leekangmin <kkk5474096@naver.com>
2025-07-09 17:00:48 +0900

    [REFACTOR/#477] ModifierExt

---
+++ core-ui/src/main/java/com/example/ui/util/ModifierExt.kt
@@ -0,0 +1,16 @@
+package com.example.ui.util
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+
+@Composable
+fun Modifier.noRippleClickable(onClick: () -> Unit): Modifier = this.then(
+    Modifier.clickable(
+        interactionSource = remember { MutableInteractionSource() },
+        indication = null,
+        onClick = onClick
+    )
+)
